0809ca1db552eb53c1d89d1084f26d538a9075c9
bbaale-edger <bbaaleedger2019@gmail.com>
2023-04-20 12:54:13 +0300

    added student table

--- erp/admission/models.py
+++ erp/admission/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.model):
+    Firstname=models.CharField(max_length=200)
+    Lastname=models.CharField(max_length=200)
+    contact=models.CharField(max_length=20)
+    Nin=models.CharField(max_length=30)
+    Photo=models.Imagefield(upload_to ="uploads/")
+    GENDER_CHOICES =[('MALE',"MALE"),
+    ("FEMALE", "FEMALE"),]
+    GENDER =models.CharField (
+        max_length=6,
+        choices=GENDER_CHOICES,
+        default=MALE,
+    )
+    DOB = models.DateField()
